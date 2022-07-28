0eae7c976d417926bf2fc4fafa9f7c8de0f3be9e
error228666 <vaneksirer@yandex.ru>
2022-07-28 16:30:47 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,151 @@
+from tensorflow.keras.models import Model
+from tensorflow import keras
+import tensorflow as tf
+import numpy as np
+import matplotlib.cm as cm
+import cv2
+
+model = tf.keras.models.load_model("mnist.h5")
+
+
+
+last_conv_layer_name =  "conv2d_1"
+
+img_path = "numbers/0.jpg"
+
+img_size = 28
+
+cam_path="results/cam0.jpg"
+
+class GradCAM:
+    def __init__(self, model, classIdx, layerName=None):
+
+        self.model = model
+        self.classIdx = classIdx
+        self.layerName = layerName
+        # if the layer name is None, attempt to automatically find
+        # the target output layer
+        if self.layerName is None:
+            self.layerName = self.find_target_layer()
+
+    def find_target_layer(self):
+        # attempt to find the final convolutional layer in the network
+        # by looping over the layers of the network in reverse order
+        for layer in reversed(self.model.layers):
+            # check to see if the layer has a 4D output
+            if len(layer.output_shape) == 4:
+                return layer.name
+        # otherwise, we could not find a 4D layer so the GradCAM
+        # algorithm cannot be applied
+        raise ValueError("Could not find 4D layer. Cannot apply GradCAM.")
+
+    def compute_heatmap(self, image, eps=1e-8):
+
+        gradModel = Model(
+            inputs=[self.model.inputs],
+            outputs=[self.model.get_layer(self.layerName).output, self.model.output])
+
+        with tf.GradientTape() as tape:
+            inputs = tf.cast(image, tf.float32)
+            (convOutputs, predictions) = gradModel(inputs)
+
+            loss = predictions[:, tf.argmax(predictions[0])]
+
+        # use automatic differentiation to compute the gradients
+        grads = tape.gradient(loss, convOutputs)
+
+        # compute the guided gradients
+        castConvOutputs = tf.cast(convOutputs > 0, "float32")
+        castGrads = tf.cast(grads > 0, "float32")
+        guidedGrads = castConvOutputs * castGrads * grads
+
+        convOutputs = convOutputs[0]
+        guidedGrads = guidedGrads[0]
+
+
+        weights = tf.reduce_mean(guidedGrads, axis=(0, 1))
+        cam = tf.reduce_sum(tf.multiply(weights, convOutputs), axis=-1)
+
+        (w, h) = (image.shape[2], image.shape[1])
+        heatmap = cv2.resize(cam.numpy(), (w, h))
+
+
+        numer = heatmap - np.min(heatmap)
+        denom = (heatmap.max() - heatmap.min()) + eps
+        heatmap = numer / denom
+        heatmap = (heatmap * 255).astype("uint8")
+        return heatmap
+
+    def save_and_display_gradcam(self, img_path, heatmap, cam_path, alpha=0.5):
+        # Load the original image
+        img = keras.preprocessing.image.load_img(img_path)
+        img = keras.preprocessing.image.img_to_array(img)
+
+        # Rescale heatmap to a range 0-255
+        heatmap = np.uint8(255 * heatmap)
+
+        # Use jet colormap to colorize heatmap
+        jet = cm.get_cmap("jet")
+
+        # Use RGB values of the colormap
+        jet_colors = jet(np.arange(256))[:, :3]
+        jet_heatmap = jet_colors[heatmap]
+
+        # Create an image with RGB colorized heatmap
+        jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+        jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+        jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
+
+        # Superimpose the heatmap on original image
+        superimposed_img = jet_heatmap * alpha + img
+        superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+
+        # Save the superimposed image
+        superimposed_img.save(cam_path)
+
+
+image = cv2.imread(str(img_path))
+image = cv2.resize(image, (img_size, img_size))
+
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = np.dstack([image])
+image = image.astype('float32') / 255
+image = np.expand_dims(image, axis=0)
+
+preds = model.predict(image)
+i = np.argmax(preds[0])
+
+
+icam = GradCAM(model, i, last_conv_layer_name)
+heatmap = icam.compute_heatmap(image)
+
+heatmap = cv2.resize(heatmap, (img_size, img_size))
+
+def save_and_display_gradcam(img_path, heatmap, cam_path, alpha=0.5):
+    # Load the original image
+    img = keras.preprocessing.image.load_img(img_path)
+    img = keras.preprocessing.image.img_to_array(img)
+
+    # Rescale heatmap to a range 0-255
+    heatmap = np.uint8(255 * heatmap)
+
+    # Use jet colormap to colorize heatmap
+    jet = cm.get_cmap("jet")
+
+    # Use RGB values of the colormap
+    jet_colors = jet(np.arange(256))[:, :3]
+    jet_heatmap = jet_colors[heatmap]
+
+    # Create an image with RGB colorized heatmap
+    jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+    jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
+
+    # Superimpose the heatmap on original image
+    superimposed_img = jet_heatmap * alpha + img
+    superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+
+    # Save the superimposed image
+    superimposed_img.save(cam_path)
+
+save_and_display_gradcam(img_path, heatmap, cam_path)
